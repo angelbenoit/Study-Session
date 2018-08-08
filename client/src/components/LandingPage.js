977d78cf27cd1319dec104d152b7f65f57c07b28
Angel Contreras <angelcbenoit@gmail.com>
2018-08-08 11:23:28 -0700

    restyle landing page. Show features

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -41,7 +41,7 @@ class LandingPage extends Component {
 
                         <i
                             class="fa fa-arrow-circle-down landing-page__header-scroll"
-                            onClick={() => scroll.scrollTo(350)}
+                            onClick={() => scroll.scrollTo(800)}
                         >
                         </i>
                     </div>
@@ -51,33 +51,46 @@ class LandingPage extends Component {
                     <div className="landing-page__details-footerIntro">
                         <h1>Features</h1>
                         <div className="feature_list">
+
                             <div className="feature_list-item">
-                                <h4>Schedule with our built in calendar</h4>
-                                <p>
-                                    Using the calendar, users can schedule a study Session
-                                    any day of the year.
-                                </p>
+                                <div className="feature_list-item-text">
+                                    <h4>Schedule with our built in calendar</h4>
+                                    <p>
+                                        Using the calendar, users can schedule a study Session
+                                        any day of the year.
+                                    </p>
+                                </div>
+                                <img src={require("../images/calendar.png")} alt="test"/>
                             </div>
+
                             <div className="feature_list-item">
-                                <h4>Starting the timed study session</h4>
-                                <p>
-                                    You can only access the study session they scheduled
-                                    on that specific date. Just click on start session
-                                    and you'll be able to see the remaining subjects needed
-                                    to be completed. Once you start a session for a subject
-                                    on a particular day, you will have to complete it without pausing.
-                                    Once a session for a particular subject has been completed,
-                                    it will be marked completed, and you will have the option to
-                                    move on to start the next subject or just take a break.
-                                </p>
+                                <div className="feature_list-item-text">
+                                    <h4>Starting the timed study session</h4>
+                                    <p>
+                                        You can only access the study session they scheduled
+                                        on that specific date. Just click on start session
+                                        and you'll be able to see the remaining subjects needed
+                                        to be completed. Once you start a session for a subject
+                                        on a particular day, you will have to complete it without pausing.
+                                        Once a session for a particular subject has been completed,
+                                        it will be marked completed, and you will have the option to
+                                        move on to start the next subject or just take a break.
+                                    </p>
+                                </div>
+                                <img src={require("../images/landing.jpg")} alt="test"/>
                             </div>
+
                             <div className="feature_list-item">
-                                <h4>User Dashboard</h4>
-                                <p>
-                                    See your progress, like how many sessions you've completed.
-                                    View past sessions and set goals.
-                                </p>
+                                <div className="feature_list-item-text">
+                                    <h4>User Dashboard</h4>
+                                    <p>
+                                        See your progress, like how many sessions you've completed.
+                                        View past sessions and set goals.
+                                    </p>
+                                </div>
+                                <img src={require("../images/landing.jpg")} alt="test"/>
                             </div>
+
                         </div>
                     </div>
                 </div>
