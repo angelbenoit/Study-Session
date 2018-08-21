46e79e95976a7f0e11313b609decc3fdcec72a22
Angel Contreras <angelcbenoit@gmail.com>
2018-08-21 15:41:25 -0700

    show stats about user completion rate in overview page in the dashboard

--- client/src/components/DashboardComponents/DashboardOverview.js
+++ client/src/components/DashboardComponents/DashboardOverview.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return (
-    <div>
-        <h1>OVERVIEW</h1>
-    </div>
-  )
+class DashboardOverview extends Component {
+
+  getTotalPercentageCompleted(data){
+    if(data){
+      let completedSubjects;
+      if(data.totalSubjectsCompleted)
+        completedSubjects = data.totalSubjectsCompleted;
+      else
+        completedSubjects = 0;
+
+      const percentageCompleted = completedSubjects/data.sessions.length;
+      const overviewLook = `${completedSubjects}/${data.sessions.length}`;
+      console.log(percentageCompleted, overviewLook);
+      return(
+        <div>
+          <h4>You've Completed {percentageCompleted}% of subjects in your study sessions</h4>
+          <h4>{overviewLook}</h4>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    const overview = this.getTotalPercentageCompleted(this.props.auth)
+    return (
+      <div>
+        {overview}
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return { auth : state.auth }
+}
+
+export default connect(mapStateToProps)(DashboardOverview);
